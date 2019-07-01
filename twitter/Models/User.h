0c9a6509ef6359f9eebed2b11df26756cc6a9324
José Antonio Murillo <josemurillo@fb.com>
2019-07-01 16:53:47 -0700

    finished first task

---
+++ twitter/Models/User.h
@@ -0,0 +1,24 @@
+//
+//  User.h
+//  twitter
+//
+//  Created by josemurillo on 7/1/19.
+//  Copyright © 2019 Emerson Malca. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface User : NSObject
+
+// Basic user features
+@property (strong, nonatomic) NSString *name;
+@property (strong, nonatomic) NSString *screenName;
+
+// Create initializer
+- (instancetype)initWithDictionary:(NSDictionary *)dictionary;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ twitter/Models/User.m
@@ -0,0 +1,24 @@
+//
+//  User.m
+//  twitter
+//
+//  Created by josemurillo on 7/1/19.
+//  Copyright © 2019 Emerson Malca. All rights reserved.
+//
+
+#import "User.h"
+
+@implementation User
+
+// Method initializaes the object and returns a user
+- (instancetype)initWithDictionary:(NSDictionary *)dictionary {
+    self = [super init];
+    if (self) {
+        // Initializes properties
+        self.name = dictionary[@"name"];
+        self.screenName = dictionary[@"screen_name"];
+    }
+    return self;
+}
+
+@end
